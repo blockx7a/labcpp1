f4da4baa1aa4d3dcdaf22c06782fe9ad935fa0a9
blockx7a <domnyshev.www@gmail.com>
2024-12-05 12:46:10 +0200

    Add files via upload

---
+++ program5.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	float pie = 3.14f;
+	int r;
+	cin>> r;
+	cout<<" довжина кола " << r*pie*2<<endl;
+	cout<<" площа круга " << pie*r*r<<endl;
+}
